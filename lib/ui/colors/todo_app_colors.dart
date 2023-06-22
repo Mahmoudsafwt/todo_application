a9110999de13df42cac8f21504338a566d25d0ba
mahmoud safwat <mahmoud safwat649@gmail.com>
2023-06-22 15:06:12 +0300

    add settings screen add theming modes

--- lib/ui/colors/todo_app_colors.dart
+++ lib/ui/colors/todo_app_colors.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 Color lightGreen = const Color(0xffDFECDB);
 Color primaryColor = const Color(0xff5D9CEC);
 Color lightGrayColor = const Color(0xffC8C9CB);
+Color white = Colors.white;
+Color black = Colors.black;
--- lib/ui/theme/my_theme.dart
+++ lib/ui/theme/my_theme.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:todo_app/ui/colors/todo_app_colors.dart';
 
 class MyTheme {
+  static Color unSelectedDay = Colors.black;
+  static Color unSelectedMonth = Colors.black;
+  static Color backgroundDays = Colors.white;
   static ThemeData lightTheme = ThemeData(
+      textTheme: TextTheme(
+        bodyLarge: GoogleFonts.poppins(
+            color: Colors.black, fontSize: 22, fontWeight: FontWeight.bold),
+        bodyMedium: GoogleFonts.poppins(
+            color: primaryColor, fontSize: 18, fontWeight: FontWeight.bold),
+        bodySmall: GoogleFonts.roboto(
+            color: Colors.black, fontSize: 14, fontWeight: FontWeight.normal),
+      ),
       scaffoldBackgroundColor: lightGreen,
       appBarTheme: AppBarTheme(color: primaryColor),
       bottomNavigationBarTheme: BottomNavigationBarThemeData(
@@ -11,4 +23,13 @@ class MyTheme {
         selectedIconTheme: IconThemeData(color: primaryColor, size: 34),
         unselectedIconTheme: IconThemeData(color: lightGrayColor, size: 34),
       ));
+  static ThemeData darkTheme = ThemeData(
+      textTheme: TextTheme(
+    bodyLarge: GoogleFonts.poppins(
+        color: Colors.white, fontSize: 22, fontWeight: FontWeight.bold),
+    bodyMedium: GoogleFonts.poppins(
+        color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+    bodySmall: GoogleFonts.roboto(
+        color: Colors.white, fontSize: 14, fontWeight: FontWeight.normal),
+  ));
 }
